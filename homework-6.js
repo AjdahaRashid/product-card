83101b77fd18eb298186d6d678621885e557a97b
Ali <dr.magomedovali>
2026-01-21 01:35:03 +0300

    corrected errors

--- homework-6.js
+++ homework-6.js
@@ -4,33 +4,33 @@ const myDetails = {
   country: "Россия",
   city: "Кизляр",
   profession: "Бизнесмен",
-  hobby:"Спорт" 
+  hobby: "Спорт" 
 }
 
 const myCar = {
-  brand:"Феррари",
-  model:"812 competizione",
-  yearOfManufacture:"2025",
-  color:"Белый",
-  transmission:"Механика"
+  brand: "Феррари",
+  model: "812 competizione",
+  yearOfManufacture: "2025",
+  color: "Белый",
+  transmission: "Механика"
 }
 myCar.owner = myDetails
 console.log(myCar);
 
 // задание 5
 
-function checkAndAddMaxSpeed(carObj) {
-  if (carObj.maxSpeed  === undefined) {
+function checkingTheMaximumSpeed(carObj) {
+  if (carObj.maxSpeed) {
     carObj.maxSpeed = 350;
   }
 }
-checkAndAddMaxSpeed(myCar);
+checkingTheMaximumSpeed(myCar);
 console.log(myCar)
 
 // задание 6
 
-function displayInfo(myCar, model) {
-  console.log(myCar[model])
+function displayInfo(obj, prop) {
+  console.log(obj[prop])
 }
 displayInfo(myCar,"model")
 
@@ -43,70 +43,64 @@ console.log(products)
 
 let books = [
   {
-    name:"Преступление и наказание",
-    author:"Федор Достоевский",
-    genre:"психологический роман",
-    yearOfPublication:"1866"
+    name: "Преступление и наказание",
+    author: "Федор Достоевский",
+    genre: "психологический роман",
+    yearOfPublication: 1866
   },
   {
-    name:"Автостопом по галатике",
-    author:"Дуглас Адамс",
-    genre:"научно-фантастическая комедия",
-    yearOfPublication:"1977"
+    name: "Автостопом по галатике",
+    author: "Дуглас Адамс",
+    genre: "научно-фантастическая комедия",
+    yearOfPublication: 1977
   },
   {
-    name:"Убийство в Восточном экспрессе",
-    author:"Агата Кристи",
-    genre:"детектив",
-    yearOfPublication:"1934"
+    name: "Убийство в Восточном экспрессе",
+    author: "Агата Кристи",
+    genre: "детектив",
+    yearOfPublication: 1934
   }
 ]
 
 // добавление четвертую книгу через push
 
 books.push({
-  name:"1984",
-  author:"Джордж Оруэл",
-  genre:"антиутопия",
-  yearOfPublication:"1949",
+  name: "1984",
+  author: "Джордж Оруэлл",
+  genre: "антиутопия",
+  yearOfPublication: 1949,
 })
 console.log(books);
 
 // 9 задание
 
-let potterBooks = [
+const potterBooks = [
   {
-    name:"Гарри Поттер и философский камень",
-    author:"Дж.К.Ролинг",
-    genre:"фэнтези",
-    yearOfPublication:"1997"
+    name: "Гарри Поттер и философский камень",
+    author: "Дж.К.Ролинг",
+    genre: "фэнтези",
+    yearOfPublication: 1997
   },
   {
-    name:"Гарри Поттер и Тайная комната",
-    author:"Дж.К.Ролинг",
-    genre:"фэнтези",
-    yearOfPublication:"1998"
+    name: "Гарри Поттер и Тайная комната",
+    author: "Дж.К.Ролинг",
+    genre: "фэнтези",
+    yearOfPublication: 1998
   },
   {
-    name:"Гарри Поттер и узник Азкабана",
-    author:"Дж.К.Ролинг",
-    genre:"фэнтези",
-    yearOfPublication:"1999"
+    name: "Гарри Поттер и узник Азкабана",
+    author: "Дж.К.Ролинг",
+    genre: "фэнтези",
+    yearOfPublication: 1999
   }
 ]
 
-let allBooks = [...books, ...potterBooks];
+const allBooks = [...books, ...potterBooks];
 console.log(allBooks)
 
 // 10 задание 
 
-let booksTagged = allBooks.map(function(books){
-  let year = Number(books.yearOfPublication);
-  if (year < 1950) {
-    books.isRare = true;
-  } else {
-    books.isRare = false;
-  }
-  return books;
-})
-console.log(booksTagged)
+const booksTagged = allBooks.map(book => ({
+  ...book,
+  isRare: book.yearOfPublication < 1950
+}));
