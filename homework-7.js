bc1694bb9ef26bc15033e146455bcd0f0fd99d0d
Ali <dr.magomedovali>
2026-02-05 02:54:41 +0300

    feature/7-homework

---
+++ homework-7.js
@@ -0,0 +1,77 @@
+// задания 2
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+const filteredNumbers = numbers.filter(number => number >= 5)
+console.log(filteredNumbers)
+
+// задание 3
+
+const cars = ["mercedes","bmw","audi"]
+const newArray = cars.includes("jeep")
+console.log(newArray)
+
+// задание 4 
+
+function reverse(arr) {
+  return arr.reverse();
+}
+
+const reversedNumbers = reverse(filteredNumbers);
+const reversedCars = reverse(cars);
+console.log (filteredNumbers)
+console.log (cars)
+
+// задание 6
+
+import { socialComments } from "./comments.js";
+console.log(socialComments)
+
+// задание 7
+
+const authorsWithComEmail = socialComments.filter(authorData => authorData.email.includes(".com"))
+console.log(authorsWithComEmail)
+
+// задание 8
+
+const updatedComments = socialComments.map(comment => ({
+  ...comment,
+  postId: comment.id <= 5 ? 2 : 1
+}));
+console.log("Updated comments:", updatedComments);
+
+// задание 9
+
+const cards = socialComments.map(profile => {
+  return {
+    id: profile.id,
+    name: profile.name
+  };
+})
+console.log(cards)
+
+// задание 10 
+
+const checkedCommets = socialComments.map(item => ({
+  ...item,
+  isInvalid: item.body.length > 180 
+}));
+console.log(checkedCommets)
+
+// задание 11
+
+const emailsReduce = socialComments.reduce((acc, comment) => {
+  acc.push(comment.email);
+  return acc;
+}, []);
+
+console.log(emailsReduce)
+
+const emailsMap = socialComments.map(comment => comment.email);
+console.log(emailsMap)
+
+// задание 12
+
+const emailsString = socialComments.toString();
+console.log(emailsString)
+
+const emailsJoin = socialComments.join(1);
+console.log(emailsJoin)
