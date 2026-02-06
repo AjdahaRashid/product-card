28f44d8160e03f6a5f498791c8e504e58f5f49b9
Ali <dr.magomedovali>
2026-02-07 01:44:21 +0300

    fixed the errors2

--- homework-7.js
+++ homework-7.js
@@ -1,17 +1,17 @@
 import { socialComments } from "./comments.js";
-console.log(socialComments)
+console.log(socialComments);
 
 
 // задания 2
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const filteredNumbers = numbers.filter(number => number >= 5)
-console.log(filteredNumbers)
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const filteredNumbers = numbers.filter(number => number >= 5);
+console.log(filteredNumbers);
 
 // задание 3
 
-const cars = ["mercedes", "bmw", "audi"]
-const newArray = cars.includes("jeep")
-console.log(newArray)
+const cars = ["mercedes", "bmw", "audi"];
+const newArray = cars.includes("jeep");
+console.log(newArray);
 
 // задание 4 
 
@@ -21,8 +21,8 @@ function reverseArray(arr) {
 
 const reversedNumbers = reverseArray(filteredNumbers);
 const reversedCars = reverseArray(cars);
-console.log(filteredNumbers)
-console.log(cars)
+console.log(filteredNumbers);
+console.log(cars);
 
 // задание 7
 
@@ -35,7 +35,7 @@ const updatedComments = socialComments.map(comment => ({
   ...comment,
   postId: comment.id <= 5 ? 2 : 1
 }));
-console.log("Updated comments:", updatedComments);
+console.log(updatedComments);
 
 // задание 9
 
@@ -45,7 +45,7 @@ const cards = socialComments.map(profile => {
     name: profile.name
   };
 })
-console.log(cards)
+console.log(cards);
 
 // задание 10 
 
@@ -53,7 +53,7 @@ const checkedCommets = socialComments.map(item => ({
   ...item,
   isInvalid: item.body.length > 180
 }));
-console.log(checkedCommets)
+console.log(checkedCommets);
 
 // задание 11
 
@@ -62,15 +62,15 @@ const emailsReduce = socialComments.reduce((acc, comment) => {
   return acc;
 }, []);
 
-console.log(emailsReduce)
+console.log(emailsReduce);
 
 const emailsMap = socialComments.map(comment => comment.email);
-console.log(emailsMap)
+console.log(emailsMap);
 
 // задание 12
 
 const emailsString = socialComments.toString();
-console.log(emailsString)
+console.log(emailsString);
 
 const emailsJoin = socialComments.join(1);
-console.log(emailsJoin)
+console.log(emailsJoin);
