033e0e4f5067f3da99e3ade1fbffd2c631970a7a
Ali <dr.magomedovali>
2026-04-18 17:32:48 +0300

    corrected errors

--- Tea.js
+++ Tea.js
@@ -1,8 +1,9 @@
 import { Drink } from "./drink.js";
+
 export class Tea extends Drink {
-  constructor(name,size,price,temperature,teaType) {
-    super(name,size,price,temperature);
-    this.teaType = teaType
+  constructor(name, size, price, temperature, teaType) {
+    super(name, size, price, temperature);
+    this.teaType = teaType;
   }
   getInfo() {
     return `${super.getInfo()}, Сорт:${this.teaType}`;
